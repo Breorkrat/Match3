b7bc69a16273be13441b697a7eb5856aca0a7fd9
Breorkrat <mkrzesinskidasilva@gmail.com>
2024-12-29 16:06:04 -0300

    Menu de pausa e criação de níveis

--- main.c
+++ main.c
@@ -3,20 +3,21 @@
 #include <time.h> 
 #include "include/logic.h"
 
-typedef enum Telas { LOGO = 0, MENU, JOGO } Telas;
-
 int main(void)
 {
     SetRandomSeed(time(NULL));
     game tabuleiro;
     
-    inicializarJogo(&tabuleiro);
     Telas tela = LOGO;
     InitWindow(LARGURA, ALTURA, "Match3");
     
     background = LoadTexture("images/background.png");
     SetTargetFPS(60);
     int timerMenu = 0;
+
+    Rectangle botãoJogar = {LARGURA/2-400, 160, 310, 80};
+    Rectangle botãoNivel = {LARGURA/2-400, 285, 310, 80};
+    Rectangle botãoSair = {LARGURA/2-330, 410, 180, 80};
 
     while (!WindowShouldClose())
     {
@@ -41,16 +42,24 @@ int main(void)
                 DrawRectangle(0, 0, LARGURA, ALTURA, (Color){0, 0, 0, 100});
                 DrawText("Match 3", LARGURA/2-360, 40, 65, WHITE);
 
-                Rectangle botãoJogar = {LARGURA/2-330, 160, 180, 80};
-                DrawRectangle(LARGURA/2-330, 160, 180, 80, BLACK);
-                DrawText("Jogar", LARGURA/2-300, 180, 40, WHITE);
+                DrawRectangle(LARGURA/2-400, 160, 310, 80, BLACK);
+                DrawText("Novo Jogo", LARGURA/2-350, 180, 40, WHITE);
                 if (CheckCollisionPointRec(GetMousePosition(), botãoJogar)) {
                     DrawRectangleLinesEx(botãoJogar, 4, WHITE);
-                    if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
+                    if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
+                        inicializarJogo(&tabuleiro);
                         tela = JOGO;
+                    }
                 } else DrawRectangleLinesEx(botãoJogar, 2, LIGHTGRAY);
 
-                Rectangle botãoSair = {LARGURA/2-330, 410, 180, 80};
+                DrawRectangle(LARGURA/2-400, 285, 310, 80, BLACK);
+                DrawText("Escolher nível", LARGURA/2-390, 305, 40, WHITE);
+                if (CheckCollisionPointRec(GetMousePosition(), botãoNivel)) {
+                    DrawRectangleLinesEx(botãoNivel, 4, WHITE);
+                    if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
+                        tela = NIVEIS;
+                } else DrawRectangleLinesEx(botãoNivel, 2, LIGHTGRAY);
+
                 DrawRectangle(LARGURA/2-330, 410, 180, 80, BLACK);
                 DrawText("Sair", LARGURA/2-280, 430, 40, WHITE);
                 if (CheckCollisionPointRec(GetMousePosition(), botãoSair)) {
@@ -63,19 +72,72 @@ int main(void)
                 break;
 
             case JOGO:
-                if(IsKeyPressed(KEY_ENTER)) tela = MENU;
+                if(IsKeyPressed(KEY_F1)) {
+                    modo_edicao = !modo_edicao;
+                }
+                int pecaAtual = tabuleiro.tabuleiro[tabuleiro.cursor[0]][tabuleiro.cursor[1]];
+                if(modo_edicao && IsKeyPressed(KEY_Z) && pecaAtual > 0) tabuleiro.tabuleiro[tabuleiro.cursor[0]][tabuleiro.cursor[1]]--;
+                if(modo_edicao && IsKeyPressed(KEY_X) && pecaAtual < NUMTIPOS) tabuleiro.tabuleiro[tabuleiro.cursor[0]][tabuleiro.cursor[1]]++;
+                if(modo_edicao && IsKeyPressed(KEY_F2)) {
+                    FILE* save = fopen("saves/save.m3s", "wb");
+                    if (save == NULL) return 2;
+                    fwrite(&tabuleiro, sizeof(tabuleiro), 1, save);
+                    fclose(save);
+                }
+                if(IsKeyPressed(KEY_ENTER)) tela = PAUSE;
                 tabuleiro.cursor[0] = (GetMouseX()-(MARGEM_JANELA_LARGURA/2))/LADO;
                 tabuleiro.cursor[1] = (GetMouseY()-ALTURA_HUD)/LADO;
-                if(IsKeyPressed(KEY_Z)) tabuleiro.tabuleiro[tabuleiro.cursor[0]][tabuleiro.cursor[1]]--;
-                if(IsKeyPressed(KEY_X)) tabuleiro.tabuleiro[tabuleiro.cursor[0]][tabuleiro.cursor[1]]++;
-                updateMatches(&tabuleiro);
+                if (!modo_edicao) updateMatches(&tabuleiro);
                 draw(&tabuleiro);
+                break;
+
+            case NIVEIS:
+                BeginDrawing();
+                ClearBackground(BLACK);
+                DrawTexture(background, 0, 0, WHITE);
+                DrawRectangle(0, 0, LARGURA, ALTURA, (Color){0, 0, 0, 100});
+                DrawText("Arraste o arquivo de jogo aqui", LARGURA/2-500, 40, 30, WHITE);
+
+                if (IsFileDropped()){
+                    FilePathList listaArquivos = LoadDroppedFiles();
+                    char nomeArquivo[100];
+                    TextCopy(nomeArquivo, listaArquivos.paths[0]);
+                    FILE *nivel = fopen(nomeArquivo, "rb");
+                    if (nivel == NULL) return 3;
+                    fread(&tabuleiro, sizeof(tabuleiro), 1, nivel);
+                    fclose(nivel);
+                    tela = JOGO;
+                }
+                EndDrawing();
+                break;
+
+            case PAUSE:
+                BeginDrawing();
+                ClearBackground(BLACK);
+                DrawTexture(background, 0, 0, WHITE);
+                DrawRectangle(0, 0, LARGURA, ALTURA, (Color){0, 0, 0, 100});
+                DrawText("PAUSE", LARGURA/2-360, 40, 65, WHITE);
+
+                
+                DrawRectangle(LARGURA/2-400, 160, 310, 80, BLACK);
+                DrawText("Continuar", LARGURA/2-345, 180, 40, WHITE);
+                if (CheckCollisionPointRec(GetMousePosition(), botãoJogar)) {
+                    DrawRectangleLinesEx(botãoJogar, 4, WHITE);
+                    if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
+                        tela = JOGO;
+                    }
+                } else DrawRectangleLinesEx(botãoJogar, 2, LIGHTGRAY);
+                DrawRectangle(LARGURA/2-400, 285, 310, 80, BLACK);
+                DrawText("Menu Inicial", LARGURA/2-360, 305, 40, WHITE);
+                if (CheckCollisionPointRec(GetMousePosition(), botãoNivel)) {
+                    DrawRectangleLinesEx(botãoNivel, 4, WHITE);
+                    if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
+                        tela = MENU;
+                } else DrawRectangleLinesEx(botãoNivel, 2, LIGHTGRAY);
+                EndDrawing();
                 break;
         }
     }
     CloseWindow();
-
-
-
     return 0;
 }
